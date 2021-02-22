9a6f6d8afe9ff7768adcdabaef565f72c4e3dd95
VokunGahrotLaas <maxence.michot.pro@gmail.com>
2021-02-22 22:48:27 +0100

    commented AClient & AServer & switched to more specific using directives

--- Assets/Scripts/Network/AClient.cs
+++ Assets/Scripts/Network/AClient.cs
@@ -1,36 +1,65 @@
-using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading;
-
-using Network.Packet;
+using IDisposable = System.IDisposable;
+using GC = System.GC;
+using Array = System.Array;
+using BytesStack = System.Collections.Generic.Stack<byte[]>;
+using IPEndPoint = System.Net.IPEndPoint;
+using TcpClient = System.Net.Sockets.TcpClient;
+using UdpClient = System.Net.Sockets.UdpClient;
+using NetworkStream = System.Net.Sockets.NetworkStream;
+using IOControlCode = System.Net.Sockets.IOControlCode;
+using Thread = System.Threading.Thread;
+using APacket = Network.Packet.APacket;
 
 namespace Network {
+	/// <summary>
+	/// abstract class defining the behaviour of a client
+	/// interface between packets and network
+	/// implements <see cref="IDisposable"/>
+	/// </summary>
 	public abstract class AClient: IDisposable {
+		/// <summary> has the object already been disposed of ? </summary>
 		private bool _disposed;
+		/// <summary> TCP client </summary>
 		private readonly TcpClient _tcpClient;
+		/// <summary> UDP client </summary>
 		private readonly UdpClient _udpClient;
+		/// <summary> buffer for receiving TCP packets </summary>
 		private readonly byte[] _tcpBuffer = new byte[2048];
+		/// <summary> TCP client's stream </summary>
 		private readonly NetworkStream _tcpStream;
-		private readonly Stack<byte[]> _tcpStack = new Stack<byte[]>();
-		private readonly Stack<byte[]> _udpStack = new Stack<byte[]>();
-		private readonly Stack<byte[]> _receiveStack = new Stack<byte[]>();
+		/// <summary> stack for packets to send with TCP </summary>
+		private readonly BytesStack _tcpStack = new BytesStack();
+		/// <summary> stack for packets to send with UDP </summary>
+		private readonly BytesStack _udpStack = new BytesStack();
+		/// <summary> stack for received packets </summary>
+		private readonly BytesStack _receiveStack = new BytesStack();
+		/// <summary> client's execution thread <seealso cref="Run"/> </summary>
 		private readonly Thread _thread;
 
+		/// <summary> contains IP address of the remote host </summary>
 		public IPEndPoint EndPoint { get; }
+		/// <summary> is the client running ? </summary>
 		public bool IsOpen { get; private set; }
+		/// <summary> is the client connected to a remote host ? </summary>
 		public bool Connected => _tcpClient.Connected;
+		/// <summary> are packets available ? </summary>
 		public bool Available => _tcpClient.Available != 0 || _udpClient.Available != 0;
 
+		/// <summary> creates and connects a TCP client from an address </summary>
+		/// <param name="endPoint"> address of the remote host </param>
+		/// <returns> the created TCP client </returns>
 		private static TcpClient TcpClientFromEndPoint(IPEndPoint endPoint) {
 			TcpClient client = new TcpClient();
 			client.Connect(endPoint);
 			return client;
 		}
 
+		/// <summary> constructs a client from a remote address </summary>
+		/// <param name="endPoint"> address of the remote host </param>
 		protected AClient(IPEndPoint endPoint): this(TcpClientFromEndPoint(endPoint)) { }
-
+		
+		/// <summary> constructs a client from a TCP client </summary>
+		/// <param name="tcpClient"> TCP client </param>
 		protected AClient(TcpClient tcpClient) {
 			_tcpClient = tcpClient;
 			EndPoint = (IPEndPoint)_tcpClient.Client.RemoteEndPoint;
@@ -48,6 +77,11 @@ namespace Network {
 			_thread = new Thread(Run);
 		}
 
+		/// <summary>
+		/// function ran by the client's thread <see cref="_thread"/>
+		/// receives and sends packets
+		/// future: track disconnections (keep alive packets)
+		/// </summary>
 		private void Run() {
 			while (IsOpen) {
 				if (_tcpStack.Count > 0) {
@@ -75,13 +109,16 @@ namespace Network {
 			}
 		}
 
+		/// <summary> custom destructor to implement <see cref="IDisposable"/> </summary>
 		~AClient() => Dispose(false);
 		
+		/// <summary> implementing <see cref="IDisposable"/> </summary>
 		public void Dispose() {
 			Dispose(true);
 			GC.SuppressFinalize(this);
 		}
 		
+		/// <summary> implementing <see cref="IDisposable"/> </summary>
 		protected virtual void Dispose(bool disposing) {
 			if (_disposed) return;
 			_disposed = true;
@@ -92,6 +129,7 @@ namespace Network {
 			_tcpStream.Dispose();
 		}
 
+		/// <summary> starts the client's thread </summary>
 		public void Start() {
 			if (IsOpen) return;
 			IsOpen = true;
@@ -99,6 +137,10 @@ namespace Network {
 			_thread.Start();
 		}
 
+		/// <summary>
+		/// closes the client's thread
+		/// <seealso cref="ForceClose"/>
+		/// </summary>
 		public void Close() {
 			if (!IsOpen) return;
 			OnClose();
@@ -108,6 +150,10 @@ namespace Network {
 			_udpClient.Close();
 		}
 
+		/// <summary>
+		/// forces the client's thread to close
+		/// <seealso cref="Close"/>
+		/// </summary>
 		public void ForceClose() {
 			if (!IsOpen) return;
 			IsOpen = false;
@@ -116,14 +162,19 @@ namespace Network {
 			_udpClient.Close();
 		}
 
+		/// <summary> tries to reconnect to a lost host and closes if failure to do so </summary>
 		private void Disconnect() {
 			if (!OnDisconnect())
 				Close();
 		}
 
+		/// <summary> send a packet with TCP </summary>
+		/// <param name="packet"> packet to send </param>
 		public void SendTcp(APacket packet)
 			=> _tcpStack.Push(packet.Read());
-
+		
+		/// <summary> send a packet with UDP </summary>
+		/// <param name="packet"> packet to send </param>
 		public void SendUdp(APacket packet)
 			=> _udpStack.Push(packet.Read());
 
@@ -135,9 +186,13 @@ namespace Network {
 		public TPacket Receive<TPacket>() where TPacket: APacket, new()
 			=> (TPacket)Receive(new TPacket());
 
+		/// <summary> on start event <seealso cref="Start"/> </summary>
 		protected virtual void OnStart() { }
+		/// <summary> on close event <seealso cref="Close"/> </summary>
 		protected virtual void OnClose() { }
+		/// <summary> on disconnect event <seealso cref="Disconnect"/> </summary>
 		protected virtual bool OnDisconnect() => false;
+		/// <summary> on receive event <seealso cref="Receive"/> </summary>
 		protected virtual void OnReceive() { }
 	}
 }
