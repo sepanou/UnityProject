a7573efe2757ee215e80dde40ce6258c768b8e25
Maxence MICHOT <maxence.michot.pro@gmail.com>
2021-03-01 22:12:23 +0100

    cleanup

--- Assets/Scripts/TempMovePlayer.cs
+++ Assets/Scripts/TempMovePlayer.cs
@@ -2,25 +2,25 @@ using UnityEngine;
 using Mirror;
 
 public class TempMovePlayer: NetworkBehaviour {
-    private Rigidbody2D _body;
-
-    public Camera _playerCamera;
-    
-    [SerializeField] private float velocity;
-    
-    void Start() {
-        _body = GetComponent<Rigidbody2D>();
-        
-        _playerCamera.gameObject.SetActive(isLocalPlayer);
-    }
-    
-    void FixedUpdate() {
-        if (isLocalPlayer) {
-            float x = Input.GetAxisRaw("Horizontal");
-            float y = Input.GetAxisRaw("Vertical");
-            Vector2 direction = new Vector2(x, y);
-            direction.Normalize();
-            _body.velocity = velocity * direction;
-        }
-    }
+	private Rigidbody2D _body;
+	public Camera playerCamera;
+	
+	[SerializeField]
+	private float velocity;
+	
+	private void Start() {
+		_body = GetComponent<Rigidbody2D>();
+		playerCamera.gameObject.SetActive(isLocalPlayer);
+	}
+	
+	private void FixedUpdate() {
+		if (isLocalPlayer) {
+			float x = Input.GetAxisRaw("Horizontal");
+			float y = Input.GetAxisRaw("Vertical");
+			Vector2 direction = new Vector2(x, y);
+			direction.Normalize();
+			_body.AddForce(velocity * direction);
+			// _body.velocity = velocity * direction;
+		}
+	}
 }
