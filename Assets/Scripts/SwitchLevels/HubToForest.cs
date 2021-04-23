783d7e2320777aff9184733dcb4d97380316c8d4
LOICVAL42 <loic.michalon@epita.fr>
2021-04-23 11:58:43 +0200

    ajout 1er trigger

---
+++ Assets/Scripts/SwitchLevels/HubToForest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UI_Audio;
+
+public class HubToForest : MonoBehaviour
+{
+    [Header("Sound Settings")]
+    [SerializeField] private AudioDB audioDB;
+    private void OnTriggerEnter(Collider other)
+    {
+        NetworkManager.singleton.ServerChangeScene("Level1Creation");
+        audioDB.PlayMusic("ForestMusic");
+        Debug.Log("Changed scene !");
+    }
+}
