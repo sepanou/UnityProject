c4047169b9f5064c86374bc283136b1da484c232
LOICVAL42 <loic.michalon@epita.fr>
2021-04-24 21:08:02 +0200

    UI des touches

---
+++ Assets/Scripts/DataBanks/ControlsUIDB.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ControlsUIDB", menuName = "DataBanks/ControlsUIDB", order = 10)]
+public class ControlsUIBD : ScriptableObject
+{
+    
+}
