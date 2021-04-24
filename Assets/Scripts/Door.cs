12406e3b81243d12c636f6ae139c50db41d41c32
LOICVAL42 <loic.michalon@epita.fr>
2021-04-24 20:08:09 +0200

    la porte fonctionne

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,4 +1,10 @@
-﻿using Mirror;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DataBanks;
+using Entity.DynamicEntity.LivingEntity.Player;
+using Mirror;
+using UI_Audio;
 using UnityEngine;
 
 public class Door: NetworkBehaviour {
@@ -8,19 +14,72 @@ public class Door: NetworkBehaviour {
 	[SerializeField] private bool isOpen;
 	[SerializeField] private Sprite closed;
 	[SerializeField] private Sprite opened;
+	private Dictionary<Player, bool> _playerPool;
+	private bool _canInteract;
+	[NonSerialized] public static InputManager InputManager;
+	[NonSerialized] public static PlayerInfoManager InfoManager;
+	
 
-	private void Start() {
+	private void Start()
+	{
 		_doorCollider = GetComponents<Collider2D>();
 		_spriteRenderer = GetComponent<SpriteRenderer>();
+		_playerPool = new Dictionary<Player, bool>();
+		_canInteract = false;
 	}
 
+	/*
 	private void OnTriggerStay2D(Collider2D other) {
 		if (LocalGameManager.Instance.inputManager.GetKeyDown("Interact"))
 			ToggleDoor();
+	}*/
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (!other.gameObject.TryGetComponent(out Player player))
+			return;
+            
+		if (isServer)
+			_playerPool[player] = false;
+
+		if (!player.isLocalPlayer) return;
+            
+		_canInteract = true;
+		StartCoroutine(CheckInteraction(player));
+	}
+        
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (!other.gameObject.TryGetComponent(out Player player))
+			return;
+            
+		if (isServer)
+			_playerPool.Remove(player);
+
+		if (player.isLocalPlayer)
+			_canInteract = false;
+	}
+	
+	[ClientCallback]
+	private IEnumerator CheckInteraction(Player player)
+	{
+		while (_canInteract)
+		{
+			if (InputManager.GetKeyDown("Interact"))
+			{
+				Debug.Log("bientot");
+				ToggleDoor();
+				yield return null;
+			}
+
+			yield return null;
+		}
 	}
 	
 	[Command(requiresAuthority = false)]
-	private void ToggleDoor() {
+	private void ToggleDoor()
+	{
+		if (!_canInteract) return;
 		_doorCollider[0].enabled = isOpen;
 		ToggleSprite(isOpen);
 		isOpen = !isOpen;
