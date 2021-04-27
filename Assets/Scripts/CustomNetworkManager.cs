ef43a4b516084624acd2a61da838e6acc48091df
Maxence MICHOT <maxence.michot.pro@gmail.com>
2021-04-27 14:11:39 +0200

    refracto

--- Assets/Scripts/CustomNetworkManager.cs
+++ Assets/Scripts/CustomNetworkManager.cs
@@ -4,57 +4,56 @@ using Mirror;
 using UI_Audio;
 using UnityEngine;
 
-public class CustomNetworkManager : NetworkManager
-{
-    public new static CustomNetworkManager singleton;
-    
-    [Header("Scene transition")] 
-    [SerializeField] private Animator sceneAnimator;
-    
-    [Header("Forest Scene Spawn Points")]
-    [SerializeField] private string forestSceneName;
-    [SerializeField] private Vector3[] forestSpawnPoints;
+public class CustomNetworkManager: NetworkManager {
+	public new static CustomNetworkManager singleton;
+	
+	[Header("Scene transition")] 
+	[SerializeField] private Animator sceneAnimator;
+	
+	[Header("Forest Scene Spawn Points")]
+	[SerializeField] private string forestSceneName;
+	[SerializeField] private Vector3[] forestSpawnPoints;
 
-    private List<Player> _playerPrefabs;
+	private List<Player> _playerPrefabs;
 
-    public override void Start() {
-        _playerPrefabs = new List<Player>();
-        base.Start();
-        singleton = this;
-    }
+	public override void Start() {
+		_playerPrefabs = new List<Player>();
+		base.Start();
+		singleton = this;
+	}
 
-    public void StartSceneTransition() => sceneAnimator.Play("StartTransition");
+	public void StartSceneTransition() => sceneAnimator.Play("StartTransition");
 
-    private void SetPlayerSpawnPoints(Vector3[] spawnPoints) {
-        for (int i = 0; i < _playerPrefabs.Count; i++)
-            _playerPrefabs[i].Position = spawnPoints[i % spawnPoints.Length];
-    }
-    
-    public override void OnServerAddPlayer(NetworkConnection conn) {
-        base.OnServerAddPlayer(conn);
-        _playerPrefabs.Add(conn.identity.gameObject.GetComponent<Player>());
-    }
+	private void SetPlayerSpawnPoints(Vector3[] spawnPoints) {
+		for (int i = 0; i < _playerPrefabs.Count; i++)
+			_playerPrefabs[i].Position = spawnPoints[i % spawnPoints.Length];
+	}
+	
+	public override void OnServerAddPlayer(NetworkConnection conn) {
+		base.OnServerAddPlayer(conn);
+		_playerPrefabs.Add(conn.identity.gameObject.GetComponent<Player>());
+	}
 
-    public override void OnServerSceneChanged(string sceneName) {
-        base.OnServerSceneChanged(sceneName);
-        if (sceneAnimator)
-            sceneAnimator.Play("EndTransition");
-        if (networkSceneName != forestSceneName) return;
-        AudioDB.PlayMusic("ForestMusic");
-        SetPlayerSpawnPoints(forestSpawnPoints);
-            
-    }
+	public override void OnServerSceneChanged(string sceneName) {
+		base.OnServerSceneChanged(sceneName);
+		if (sceneAnimator)
+			sceneAnimator.Play("EndTransition");
+		if (networkSceneName != forestSceneName) return;
+		AudioDB.PlayMusic("ForestMusic");
+		SetPlayerSpawnPoints(forestSpawnPoints);
+			
+	}
 
-    public override void OnClientSceneChanged(NetworkConnection conn) {
-        base.OnClientSceneChanged(conn);
-        if (sceneAnimator)
-            sceneAnimator.Play("EndTransition");
-        if (networkSceneName == forestSceneName)
-            AudioDB.PlayMusic("ForestMusic");
-    }
+	public override void OnClientSceneChanged(NetworkConnection conn) {
+		base.OnClientSceneChanged(conn);
+		if (sceneAnimator)
+			sceneAnimator.Play("EndTransition");
+		if (networkSceneName == forestSceneName)
+			AudioDB.PlayMusic("ForestMusic");
+	}
 
-    public override void OnStopClient() {
-        LocalGameManager.Instance.SetLocalGameState(LocalGameStates.Start);
-        base.OnStopClient();
-    }
+	public override void OnStopClient() {
+		LocalGameManager.Instance.SetLocalGameState(LocalGameStates.Start);
+		base.OnStopClient();
+	}
 }
