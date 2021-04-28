b4aa496496e50abc2a9b549973417d80017442dc
David Tchekachev <contact@tchekda.fr>
2021-04-28 03:55:51 +0200

    As always, i'm just dumb.... - v0.0.10

--- Assets/Editor/Build.cs
+++ Assets/Editor/Build.cs
@@ -4,6 +4,6 @@ public class Build {
     static void ServerBuild() {
         string[] defaultScene = {"Assets/Scenes/Hub.unity", "Assets/Scenes/Level1Creation.unity"};
         BuildPipeline.BuildPlayer(defaultScene, "./build/Linux.x86_64",
-            BuildTarget.StandaloneLinux64, BuildOptions.None);
+            BuildTarget.StandaloneLinux64, BuildOptions.EnableHeadlessMode);
     }
 }
