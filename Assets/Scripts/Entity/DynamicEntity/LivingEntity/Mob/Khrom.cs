31fe1ee05da3d88e44ee8987ed5e4aafdba59dca
Maxence MICHOT <maxence.michot.pro@gmail.com>
2021-06-15 17:49:12 +0200

    Auto stash before merge of "maxence" and "origin/loic"

--- Assets/Scripts/Entity/DynamicEntity/LivingEntity/Mob/Khrom.cs
+++ Assets/Scripts/Entity/DynamicEntity/LivingEntity/Mob/Khrom.cs
@@ -1,25 +1,26 @@
-﻿using Behaviour;
+﻿using Behaviour.Targeter;
 
 namespace Entity.DynamicEntity.LivingEntity.Mob {
     public class Khrom: Mob {
         public override int cooldown { get; protected set; } = 180;
         public override int atk { get; protected set; } = 50;
         private const float AtkMaxDist = 128;
+        private NearestPlayerTargeter targeter;
 
         private void Start() {
             Instantiate();
+            targeter = new NearestPlayerTargeter(this);
         }
 
         protected override bool CanAttack() {
-            Player.Player target = ((DistanceNearestPlayerStraightFollower)behaviour).behaviour.targeter.target;
+            Player.Player target = (Player.Player)targeter.AcquireTarget();
             return target && (target.transform.position - transform.position).sqrMagnitude < AtkMaxDist * AtkMaxDist;
         }
 
         protected override void Attack() {
-            Player.Player target = ((DistanceNearestPlayerStraightFollower)behaviour).behaviour.targeter.target;
+            Player.Player target = targeter.target;
             Animator.SetBool(isAttacking, true);
-            target.GetAttacked(atk);
-            target.Animator.SetTrigger(hasBeenHit);
+            // TODO: spawn projectile
         }
     }
 }
