e74699a2dbe4b19ba3ef376a875ec6cc8b03a808
KYN4_ <diskrazyx@gmail.com>
2021-06-16 01:48:09 +1100

    changes projectiles

--- Assets/Scripts/Entity/DynamicEntity/Projectile/Arrow.cs
+++ Assets/Scripts/Entity/DynamicEntity/Projectile/Arrow.cs
@@ -1,14 +1,5 @@
-using Mirror;
-using UnityEngine;
-
 namespace Entity.DynamicEntity.Projectile {
-	[RequireComponent(typeof(Rigidbody2D))]
 	public class Arrow: Projectile {
 		private void Start() => Instantiate();
-
-		[Server] protected override void Move() {
-			if (RigidBody.velocity != Vector2.zero) return;
-			RigidBody.velocity = FacingDirection * Speed;
-		}
 	}
 }
--- Assets/Scripts/Entity/DynamicEntity/Projectile/MagicOrb.cs
+++ Assets/Scripts/Entity/DynamicEntity/Projectile/MagicOrb.cs
@@ -1,14 +1,5 @@
-﻿using Mirror;
-using UnityEngine;
-
-namespace Entity.DynamicEntity.Projectile {
+﻿namespace Entity.DynamicEntity.Projectile {
 	public class MagicOrb: Projectile {
 		private void Start() => Instantiate();
-
-		[Server] protected override void Move() {
-			if (RigidBody.velocity != Vector2.zero) return;
-			RigidBody.velocity = FacingDirection * Speed;
-			// Add specific behaviour here -> Perlin noise?
-		}
 	}
 }
